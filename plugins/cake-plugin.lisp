bddbb7c4915692234832638e1892c565b4167adc
Johannes Knoedtel <johannes.knoedtel@informatik.stud.uni-erlangen.de>
2011-03-03 17:20:27 +0100

    Using doc-string now, instead of help method.

--- plugins/cake-plugin.lisp
+++ plugins/cake-plugin.lisp
@@ -4,18 +4,19 @@
 (defclass cake-plugin (plugin)
   ()
   (:default-initargs :name "cake"))
-
+  
 (defmethod help ((plugin cake-plugin))
-  (reply "!cake <number> : draws a crappy ASCII-art cake with <number> candles on top"))
+	(help-for-commands plugin))
 
 (defun make-cake-layer (size left-delimiter middle right-delimiter)
-  (format nil "~{~A~^~}" (append (list  left-delimiter)
-	     (loop repeat size collecting middle)
-	     (list right-delimiter))))
+	(format nil "~{~A~^~}" (append (list  left-delimiter)
+								   (loop repeat size collecting middle)
+								   (list right-delimiter))))
 
 
 
 (defcommand cake ((plugin cake-plugin) num)
-  (declare (ignore plugin))
-  (reply (cons (make-cake-layer (parse-integer  num) " _" "i_" " ")
-		 (loop repeat 3 collect (make-cake-layer (parse-integer num) "|_" "__" "|")))))
+	"!cake <number> : draws a crappy ASCII-art cake with <number> candles on top"
+	(declare (ignore plugin))
+	(reply (cons (make-cake-layer (parse-integer  num) " _" "i_" " ")
+				 (loop repeat 3 collect (make-cake-layer (parse-integer num) "|_" "__" "|")))))
