5e4e0c4281d8b83aabdd93622d599e5d26c7f05d
Hans-Peter Deifel <hpdeifel@gmx.de>
2012-01-15 19:15:48 +0100

    fix whitespace

--- http-plugins/google-plugin.lisp
+++ http-plugins/google-plugin.lisp
@@ -12,25 +12,25 @@
 
 (defun get-google-matches (search)
   (let* ((html (drakma:http-request *google-url*
-				    :parameters `(("q" . ,search))
+                                    :parameters `(("q" . ,search))
                                     :user-agent "Mozilla/5.0 (Macintosh; U; Intel Mac OS X; de-de) AppleWebKit/535+ (KHTML, like Gecko) Version/5.0 Safari/535.4+ Lispbot"))
-	 (document (chtml:parse html (cxml-stp:make-builder)))
-	 (res nil))
+         (document (chtml:parse html (cxml-stp:make-builder)))
+         (res nil))
     (stp:do-recursively (a document)
       (when (and (typep a 'stp:element)
-		 (equal (stp:local-name a) "a")
-		 (equal (stp:attribute-value a "class") "l"))
-	(push (cons (stp:string-value a)
-		    (stp:attribute-value a "href"))
-	      res)))
+                 (equal (stp:local-name a) "a")
+                 (equal (stp:attribute-value a "class") "l"))
+        (push (cons (stp:string-value a)
+                    (stp:attribute-value a "href"))
+              res)))
     (reverse res)))
 
 (defcommand google ((pl google-plugin) arg1 &rest args)
   (declare (ignore pl))
   (let* ((str (format nil "\"~a\"~{ \"~a\"~}" arg1 args))
-	 (res (get-google-matches str)))
+         (res (get-google-matches str)))
     (if res
-	(reply (format nil "~a - ~a"
-		       (car (first res))
-		       (cdr (first res))))
-	(reply (format nil "nothing found for ~a" str)))))
+        (reply (format nil "~a - ~a"
+                       (car (first res))
+                       (cdr (first res))))
+        (reply (format nil "nothing found for ~a" str)))))
