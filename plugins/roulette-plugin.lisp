50d382a9912fec5e51f8eb07ec681ef2918289e8
mark scala <markscala@gmail.com>
2013-07-16 11:10:23 -0500

    statistics operative

--- plugins/roulette-plugin.lisp
+++ plugins/roulette-plugin.lisp
@@ -12,7 +12,8 @@
 (defparameter *standing-challenge* nil)
 (defparameter *challenger* nil)
 (defparameter *challenged* nil)
-(defparameter *stats* (make-hash-table :test 'equal))
+;(defparameter *stats* '(("wag" . (0 0)) ("skalawag" . (0 0)))) ;; ((name . '(wins losses)) (name2 . '(wins losses))...)
+(defparameter *stats* nil)
 
 ;; figure out how to get the nick of the person who issues this command.
 (defcommand r ((plugin roulette-plugin) target)
@@ -27,16 +28,18 @@
          (format nil "~a has challenged ~a to Russian Roulette!" *challenger* *challenged*)))
       (reply (format nil "~a, there is a standing challenge." (nick (sender *last-message*))))))
 
+(defun reset ()
+  (setf *standing-challenge* nil)
+  (setf *challenger* nil)
+  (setf *challenged* nil))
+
 (defcommand pff ((plugin roulette-plugin))
   "accepts a challenge of roulette if a challenge has been made."
   (declare (ignore plugin))
   (cond
     ((string= (nick (sender *last-message*)) *challenged*)
      (reply (format nil "~a accepts! The game begins!" *challenged*))
-     (run-game)
-     (setq *standing-challenge* nil
-           *challenger* nil
-           *challenged* nil))
+     (run-game))
     (t (reply (format nil "~a, this is none of your business!"
                       (nick (sender *last-message*)))))))
 
@@ -47,39 +50,22 @@
         *challenged* nil
         *challenger* nil))
 
-(defun announce-result (key value)
-  (reply
-   (format nil "~a: ~a"
-           key
-           (* (float
-               (/ (first value)
-                  (+ (first value) (second value)))) 100))))
-
-
 (defcommand rstats ((plugin roulette-plugin))
   (declare (ignore plugin))
-  (maphash #'announce-result *stats*))
+  (dolist (item *stats*)
+    (reply (format nil "~a: ~a" (car item) (float (/ (car (cdr item)) (+ (car (cdr item)) (car (cdr (cdr item))))))))))
+
+(defcommand stats ((plugin roulette-plugin))
+  "this is just for diagnostics"
+  (declare (ignore plugin))
+  (reply (format nil "~a" *stats*)))
 
 (defun log-stats (name win)
   (cond
     (win
-     (cond
-       ((gethash name *stats*)
-        (let ((stats (gethash name *stats*)))
-          (setf (first stats) (1+ (first stats)))
-          (setf (gethash name *stats*) stats)))
-       (t
-        (let ((stats '(1 0)))
-          (setf (gethash name *stats*) stats)))))
-    (t
-     (cond
-       ((gethash name *stats*)
-        (let ((stats (gethash name *stats*)))
-          (setf (second stats) (1+ (second stats)))
-          (setf (gethash name *stats*) stats)))
-       (t
-        (let ((stats '(0 1)))
-          (setf (gethash name *stats*) stats)))))))
+     (setf (car (cdr (assoc name *stats* :test #'string=))) (1+ (car (cdr (assoc name *stats* :test #'string=))))))
+    ((null win)
+     (setf (car (cdr (cdr (assoc name *stats* :test #'string=)))) (1+ (car (cdr (cdr (assoc name *stats* :test #'string=)))))))))
 
 (defun run-game ()
   (let ((run-length (1+ (random 6)))
@@ -88,6 +74,10 @@
                      (list *challenged* *challenger*))))
     (reply (format nil "~a has won the coin toss!" (first players)))
     (sleep 1)
+    (if (null (assoc (first players) *stats* :test #'string=))
+        (setf *stats* (cons `(,(first players) . ,(list 0 0)) *stats*)))
+    (if (null (assoc (second players) *stats* :test #'string=))
+        (setf *stats* (cons `(,(second players) . ,(list 0 0)) *stats*)))
     (dotimes (i run-length)
       (sleep 1)
       (reply (format nil "~a spins the cylinder!" (first players)))
@@ -101,5 +91,6 @@
          (sleep 1)
          (reply (format nil "Congratulations, ~a, you have won!" (second players)))
          (log-stats (first players) nil)
-         (log-stats (second players) t)))
+         (log-stats (second players) t)
+         (reset)))
       (setf players (list (second players) (first players))))))
