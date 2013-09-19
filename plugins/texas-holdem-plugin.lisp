b6f8000d653abecf48db05ee6e7c9b8e51d2d5ff
mark scala <markscala@gmail.com>
2013-09-19 07:29:16 -0500

    added command stubs

--- plugins/texas-holdem-plugin.lisp
+++ plugins/texas-holdem-plugin.lisp
@@ -1,5 +1,37 @@
 (in-package :lispbot.plugins)
 
-(defclass poker-plugin (plugin)
+(defclass texas-holdem-plugin (plugin)
   ()
-  (:default-initargs :name "poker"))
+  (:default-initargs :name "texas-holdem"))
+
+(defcommand holdem ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand start-holdem ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand join-holdem ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand fold ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand call ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand check ((plugin poker-plugin))
+  (declare (ignore plugin))
+  nil)
+
+(defcommand bet ((plugin poker-plugin) amt)
+  (declare (ignore plugin))
+  nil)
+
+(defcommand raise ((plugin poker-plugin) amt)
+  (declare (ignore plugin))
+  nil)
